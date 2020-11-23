e02a6727490feb7e114d92c67b9aedf6b3d4f65e
Maxime Samuel <Maxime.Samuel@msss.gouv.qc.ca>
2020-11-23 13:57:46 -0500

    Added options (DO NOT USE IN PRODUCTION, not everything is tested)
    - Added Folder, iterationData, iterationCount, and insecure options
    - Refactor the main run function

--- bin/newman-run.js
+++ bin/newman-run.js
@@ -14,40 +14,59 @@ var feedFilePath = ""
 clear()
 console.log(
     chalk.rgb(220, 120, 60)(
-        figlet.textSync('Newman-Run', { 
-            font: 'Doom', 
-            horizontalLayout: 'full', 
-            whitespaceBreak: true 
+        figlet.textSync('Newman-Run', {
+            font: 'Doom',
+            horizontalLayout: 'full',
+            whitespaceBreak: true
         })
     )
 );
 
-const file_error_message = chalk.red.bold('Need either feed file (-f) or collections (-c) file to run the tests or atleast (-r) to remove the files from reports directory!!!\n')
+const file_error_message = chalk.red.bold('Need either feed file (-f) or collections (-c) file to run the tests or at least (-r) to remove the files from the reports directory!\n')
 
 const options = yargs
         .usage("Usage: newman-run -f <feed_file_path>")
         .option("f", { alias: "feed", describe: "Feed file path", type: "string"})
+        .option("d", { alias: "folder", describe: "Folder name", type: "string"})
         .option("c", { alias: "collection", describe: "Collection file path file path", type: "string"})
+        .option("t", { alias: "iterationData", describe: "Iteration data", type: "string"})
+        .option("i", { alias: "iterationCount", describe: "Iteration count", type: "number"})
+        .option("o", { alias: "insecure", describe: "Disables SSL verification checks and allows self-signed SSL certificates", type: "boolean"})
         .option("e", { alias: "environment", describe: "Environment file path file path", type: "string"})
         .option("r", { alias: "remove", describe: "To remove the files from reporting directory"})
-        .check(argv => { if(argv.f == undefined && argv.c == undefined && argv.r == undefined) { console.log(file_error_message); return false } else { return true }})
-        .argv
+        .check(argv => {
+            if(typeof argv.f === 'undefined' && typeof argv.c === 'undefined' && typeof argv.r === 'undefined') {
+                console.log(file_error_message)
+                return false
+            } else {
+                return true
+            }
+        }).argv
 
 const NC = new NewmanConfig()
 
 if (options.remove) {
     NC.clearResultsFolder()
 }
-if (options.feed != undefined && options.collection == undefined && options.environment == undefined) {
+if (typeof options.feed !== 'undefined' &&
+        typeof options.collection === 'undefined' &&
+        typeof options.environment === 'undefined') {
     NC.looprun(options.feed)
-} else if (options.collection != undefined && options.environment == undefined && options.feed == undefined) {
-    NC.runCollection(options.collection)
-} else if (options.collection != undefined && options.environment != undefined && options.feed == undefined) {
-    NC.runCollectionWithEnv(options.collection, options.environment)
-} else if (options.feed != undefined && options.collection != undefined && options.environment != undefined) {
+} else if (typeof options.collection !== 'undefined' &&
+        typeof options.feed === 'undefined') {
+    NC.runGeneric(options)
+} else if (typeof options.collection !== 'undefined' &&
+        typeof options.environment !== 'undefined' &&
+        typeof options.feed === 'undefined') {
+    NC.runGeneric(options)
+} else if (typeof options.feed !== 'undefined' &&
+        typeof options.collection !== 'undefined' &&
+        typeof options.environment != undefined) {
     NC.looprun(options.feed)
-    NC.runCollectionWithEnv(options.collection, options.environment)
-} else if (options.feed != undefined && options.collection != undefined && options.environment == undefined) {
+    NC.runGeneric(options)
+} else if (typeof options.feed !== 'undefined' &&
+        typeof options.collection !== 'undefined' &&
+        typeof options.environment === 'undefined') {
     NC.looprun(options.feed)
-    NC.runCollection(options.collection)
+    NC.runGeneric(options)
 }
